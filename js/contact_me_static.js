682e0ad11889a5856a4d0761e93915fe3061bcde
pbelouin <pbelouin@icloud.com>
2017-01-21 14:14:54 +0100

    form ajax

--- js/contact_me_static.js
+++ js/contact_me_static.js
@@ -20,4 +20,27 @@ $(function() {
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
     $('#success').html('');
+
+
+var $contactForm = $('#contactForm');
+$contactForm.submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        url: '//formspree.io/pbelouin@icloud.com',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+            $contactForm.append('<div class="alert alert-info"><i class="fa fa-circle-o-notch fa-spin fa-fw"></i>&nbsp;Sending message…</div>');
+        },
+        success: function(data) {
+            $contactForm.find('.alert-info').hide();
+            $contactForm.append('<div class="alert alert-success alert-dismissable">Message sent!<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></div>');
+        },
+        error: function(err) {
+            $contactForm.find('.alert-info').hide();
+            $contactForm.append('<div class="alert alert-error  alert-dismissable">Ops, there was an error.<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></div>');
+        }
+    });
+});
 });
